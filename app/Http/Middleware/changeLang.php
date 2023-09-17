fc0ab09c3fe3a62f7b54ef628854c61dfff85c7c
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-09-17 23:54:33 +0300

    add

--- app/Http/Middleware/changeLang.php
+++ app/Http/Middleware/changeLang.php
@@ -30,9 +30,9 @@ class changeLang
         }else{
             app()->setLocale('ar');
         }
-        if(!(request()->hasHeader('api_key') && request()->header('api_key') == env('api_key','skillar2023'))){
+       /* if(!(request()->hasHeader('api_key') && request()->header('api_key') == env('api_key','skillar2023'))){
             return messages::error_output('api key is missing !!');
-        }
+        }*/
         return $next($request);
     }
 }
