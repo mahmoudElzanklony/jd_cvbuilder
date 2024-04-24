7e6c758b07d1f5003a841d8b870c5571952ce23d
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-04-24 21:30:19 +0200

    add filter id to jobs names

--- app/Http/Controllers/Filters/IdFiltter.php
+++ app/Http/Controllers/Filters/IdFiltter.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\Filters;
-
+use Closure;
 class IdFiltter extends FilterRequest
 {
+    public function handle($request, Closure $next){
+        if (! request()->filled('id')) {
+            return $next($request);
+        }
 
+        return $next($request)
+            ->whereRaw('id > '.request('id'));
+    }
 }
